947279aa09907a2bb7c10582091a160ffab220a9
Essence Roberts <essencenroberts@gmail.com>
2026-08-14 10:18:38 -0500

    inital commit set up roject with vite, Vanilla Typescript, and TailwindCss and created vite.config.ts file and added imports needed, set up and organized folder structure for e-commer-system with models, services, and utils folders in src with Product.ts, apiService.ts, discountCalculator.ts, errorHandler.ts, and taxCalculator.ts files

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <div class="flex flex-xol items-center justify-center min-h-screen bg-slate-500 text-white">
+    <h1 class="text-4xl font-extrabold drop-shadow">Tailwind Css is working</h1>
+    <p class="mt-4 text-slate-200"> Let's Start this project, and finsih before saturday afternoon</p>
+  </div>
+</section> `
